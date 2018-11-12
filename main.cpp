54175ddb0fe352d8896a16ee0cda44cb4780e7b3
grantcoz <43943567+grantcoz@users.noreply.github.com>
2018-11-11 17:21:09 -0800

    This is the splash-screen (#34)

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include "mainwindow.hpp"
 #include <QApplication>
+#include <QSplashScreen>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSplashScreen *splash = new QSplashScreen;
+    splash->setPixmap(QPixmap(":/resources/icon.png"));
+    splash->show();
+
     MainWindow w;
+
+    QTimer::singleShot(1500,splash,SLOT(close()));
+    QTimer::singleShot(3000, &w,SLOT(show()));
+
+
     w.show();
 
     return a.exec();
