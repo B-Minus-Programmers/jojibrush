e31a9f1774750e9845e42cbabaeca9f872a38ef7
David Cruz <typokign1@gmail.com>
2018-12-06 22:50:28 -0800

    Selection sort 2.0 BABY (#73)
    * start selection sort
    
    * wip
    
    * selection sort 2.0 BABY

--- utils/algorithms.cpp
+++ utils/algorithms.cpp
@@ -1,7 +1,5 @@
 #include "algorithms.hpp"
 
-using jbrush::Comparator;
-
 namespace jbrush {
 
 int compareId(const Shape* a, const Shape* b)
@@ -17,26 +15,6 @@ int compareArea(const Shape* a, const Shape* b)
 int comparePerimeter(const Shape* a, const Shape* b)
 {
     return a->perimeter() - b->perimeter();
-}
-
-void sort(Vector<Shape*>& vector, Comparator cmp)
-{
-    if (vector.getSize() == 0) return;
-
-    uint32_t min;
-    for (uint32_t i = 0; i < vector.getSize() - 1; i++)
-    {
-        min = i;
-        for (uint32_t j = i + 1; j < vector.getSize(); j++)
-        {
-            if (cmp(vector[j], vector[min]) < 0)
-            {
-                min = j;
-            }
-        }
-
-        std::swap(vector[min], vector[i]);
-    }
 }
 
 // Moves the text stream up to the character specified
--- utils/algorithms.hpp
+++ utils/algorithms.hpp
@@ -11,12 +11,23 @@ using jbrush::Shape;
 
 namespace jbrush {
 
-using Comparator = std::function<int(const Shape*, const Shape*)>;
-
 int compareId(const Shape*, const Shape*);
 int compareArea(const Shape*, const Shape*);
 int comparePerimeter(const Shape*, const Shape*);
-void sort(Vector<Shape*>&, Comparator = compareId);
 
 void getTo(QTextStream&, const QChar&);
+
+template <typename T>
+void sort(T* start, T* end, std::function<int(const Shape*, const Shape*)> cmp)
+{
+    T* min;
+    for(T* tmp = start; tmp != end; ++tmp)
+    {
+        min = tmp;
+        for(T* tmp2 = tmp + 1; tmp2 != end; ++tmp2)
+        {
+          if(cmp(*tmp2, *min) < 0) std::swap(*tmp2, *min);
+        }
+    }
+}
 }
